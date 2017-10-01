593a07fef4b3ec0210be5b26481c3e83922dd420
Sebastiano <sebastiano.gaudeano@live.com>
2017-10-01 18:09:54 +0200

    Add Test For Employee Adapter

---
+++ EmployeeAdapterTest/EmployeeAdapterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AdpterPatternCSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeAdapterTest;
+
+namespace EmployeeAdapterTest
+{
+    [TestClass]
+    public class EmployeeAdapterTest
+    {
+        private EmployeeAdapter e;
+
+        public EmployeeAdapterTest()
+        {
+            this.e = new EmployeeAdapter(new Employee("Riccardo", "Golia")); ;
+        }
+
+        [TestMethod]
+        public void EmployeeAdapterIsContact()
+        {
+            Assert.IsTrue(e is Contact);
+        }
+
+        [TestMethod]
+        public void AdapterDoNotChangeFunctionallity()
+        {
+            Assert.IsTrue(e.FullName == "Riccardo Golia");
+        }
+    }
+}
