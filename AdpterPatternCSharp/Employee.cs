56ff596f4feb0fa12fb078f591f0431957fbc5ec
Sebastiano <sebastiano.gaudeano@live.com>
2017-10-01 17:19:44 +0200

    Add Employee class

---
+++ AdpterPatternCSharp/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdpterPatternCSharp
+{
+    public class Employee
+    {
+        private int _firstName;
+
+        public int FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        private int _lastName;
+
+        public int LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public Employee(int firstName, int lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+    }
+}
