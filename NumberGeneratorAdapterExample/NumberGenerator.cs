2a8ef491ab339c56c0c51b173211ff0a1da81c99
Sebastiano <sebastiano.gaudeano@live.com>
2017-10-01 17:51:26 +0200

    Add Number Generator Class

---
+++ NumberGeneratorAdapterExample/NumberGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGeneratorAdapterExample
+{
+    public class NumberGenerator
+    {
+        private Random _r;
+
+        public NumberGenerator()
+        {
+            _r = new Random();
+        }
+
+        public int GetNumber()
+        {
+            return _r.Next();
+        }
+    }
+}
