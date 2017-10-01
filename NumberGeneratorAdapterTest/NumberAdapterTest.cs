bb840d0c46968984d96a0c0f73964725f3c31f99
Sebastiano <sebastiano.gaudeano@live.com>
2017-10-01 18:15:48 +0200

    Add Test for Number Adapter

---
+++ NumberGeneratorAdapterTest/NumberAdapterTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberGeneratorAdapterExample;
+
+namespace NumberGeneratorAdapterTest
+{
+    [TestClass]
+    public class NumberAdapterTest
+    {
+        [TestMethod]
+        public void NumberAdapterReturnsNumber()
+        {
+            var adapter = new NumberAdapter();
+            
+            Assert.IsTrue(int.TryParse(adapter.RequestNumber(), out int _));
+        }
+    }
+}
